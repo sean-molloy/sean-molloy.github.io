ec4ac36926a0eb1ccceca379c9ad6f08d97487ce
Sean Molloy <molloy.sj@gmail.com>
2022-04-30 09:51:29 +1000

    added fade title on scroll v4

---
+++ Version 4/index.js
@@ -0,0 +1,11 @@
+const checkpoint = 600;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".front").style.opacity = opacity;
+});
